3d306f437eb113ce8a4f8c465d297d9855153924
Abdelhamid108 <abdelhamidmido10@gmail.com>
2026-09-18 15:30:42 +0300

    fix: update trivy scan script and enable scanning in Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -43,8 +43,7 @@ pipeline {
                 sh 'mvn dependency-check:check -DossindexAnalyzerEnabled=false'
               },
               "Trivy Scan": {
-                // sh "bash trivy-docker-image-scan.sh"
-                sh "echo 'trivy scan'"
+                 sh "bash trivy-docker-image-scan.sh"
               }
             )
           }
---
+++ trivy-docker-image-scan.sh
@@ -0,0 +1,16 @@
+dockerImageName=$(awk 'NR==1 (print $2)' Dockerfile)
+echo $dockerImageName
+
+docker run --rm -v $WORKSPACE/root/.cache/ aquasec/trivy:0.74.0 -q image --exit-code 0 --severity HIGH --light $dockerImageName
+docker run --rm -v $WORKSPACE/root/.cache/ aquasec/trivy:0.74.0 -q image --exit-code 1 --severity HIGH --light $dockerImageName
+
+exit_code=$?
+echo "Exit Code: $exit_code"
+ if [[ ${exit_code} == 1 ]]; then
+    echo "Image scanning failed. Vulnerabilities"
+    exit 1;
+else
+    echo "Image scanning passed successfully"
+fi;
+
+
