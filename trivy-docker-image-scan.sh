863441186631e27789436dceb43524e016c5664c
Abdelhamid108 <abdelhamidmido10@gmail.com>
2026-09-18 20:05:01 +0300

    fix: create docker volume for trivy cache

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,8 +1,11 @@
+docker volume inspect trivy-cache >/dev/null 2>&1 || \
+    docker volume create trivy-cache 
+
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
-docker run --rm -v $WORKSPACE/root/.cache/ aquasec/trivy:0.74.0 -q image --timeout 15m --exit-code 0 --severity LOW,MEDIUM,HIGH --light $dockerImageName
-docker run --rm -v $WORKSPACE/root/.cache/ aquasec/trivy:0.74.0 -q image --timeout 15m --exit-code 1 --severity CRITICAL --light $dockerImageName
+docker run --rm -v trivy-cache:/root/.cache aquasec/trivy:0.74.0 -q image --timeout 15m --exit-code 0 --severity LOW,MEDIUM,HIGH --light $dockerImageName
+docker run --rm -v trivy-cache:/root/.cache aquasec/trivy:0.74.0 -q image --timeout 15m --exit-code 1 --severity CRITICAL --light $dockerImageName
 
 exit_code=$?
 echo "Exit Code: $exit_code"
