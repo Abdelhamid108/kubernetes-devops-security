d3c2e60e04dd02d737aab212c244935a4160bfe4
Abdelhamid108 <abdelhamidmido10@gmail.com>
2026-09-19 21:50:02 +0300

    fix: k8s deployment logic error

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -3,10 +3,12 @@
 sed -i "s|replace|${imageName}|g" k8s_deployment_service.yaml
 
 # Check if deployment exists
-if ! kubectl -n default get deployment "${deploymentName}" > /dev/null; then
-    echo "Creating deployment ${deploymentName}"
-    kubectl -n default apply -f k8s_deployment_service.yaml
-else
-    echo "Updating image for ${deploymentName} to ${imageName}"
-    kubectl -n default set image deploy "${deploymentName}" "${containerName}"="${imageName}" --record=true
-fi
+# if ! kubectl -n default get deployment "${deploymentName}" > /dev/null; then
+#     echo "Creating deployment ${deploymentName}"
+#     kubectl -n default apply -f k8s_deployment_service.yaml
+# else
+#     echo "Updating image for ${deploymentName} to ${imageName}"
+#     kubectl -n default set image deploy "${deploymentName}" "${containerName}"="${imageName}" --record=true
+# fi
+
+kubectl -n default apply -f k8s_deployment_service.yaml
