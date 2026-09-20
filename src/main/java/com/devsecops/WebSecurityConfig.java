76b84bc111c138ff09f94c47e1bd7a81139ee593
Abdelhamid108 <abdelhamidmido10@gmail.com>
2026-09-21 01:53:06 +0300

    fix: update the webSecurity config class with new code for spring boot 3

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -1,22 +1,30 @@
 package com.devsecops;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.CrossOriginResourcePolicyHeaderWriter;
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .headers()
-                .contentTypeOptions()
-                .and()
+            .csrf(AbstractHttpConfigurer::disable)
+            
+            .headers(headers -> headers
+                .contentTypeOptions(contentTypeOptions -> {}) 
+                
                 .crossOriginResourcePolicy(corp -> 
                     corp.policy(CrossOriginResourcePolicyHeaderWriter.CrossOriginResourcePolicy.SAME_ORIGIN)
-                );
+                )
+            );
+
+        return http.build();
     }
 }
